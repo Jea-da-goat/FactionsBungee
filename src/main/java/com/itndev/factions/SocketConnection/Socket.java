8d609ba6661f40877c4ece846e176c82242517a3
Jea <itndevddr1@gmail.com>
2022-09-01 22:57:37 +0900

    port value and domain update

--- src/main/java/com/itndev/factions/SocketConnection/Socket.java
+++ src/main/java/com/itndev/factions/SocketConnection/Socket.java
@@ -2,8 +2,8 @@ package com.itndev.factions.SocketConnection;
 
 public class Socket {
 
-    public static String Address = "localhost";
-    public static Integer Port = 9999;
+    public static String Address = "backend.itndev.com";
+    public static Integer Port = 14233;
 
     public static Boolean Connected = true;
 }
